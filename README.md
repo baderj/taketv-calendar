36dc9fafbaaa0931b7175f099006936778016a13
Johannes Bader <github@johannesbader.ch>
2013-10-25 19:13:38 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-taketv-calendar
+TakeTV Calendar
 ===============
-
-Fetches streaming dates from TakeTV.net and writes them to an iCal-file
+Fetches streaming dates from TakeTV.net and writes them to an iCal-file. See http://www.johannesbader.ch/?p=445 for a german description.
---
+++ ical.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from icalendar import Calendar, Event
+from datetime import datetime, timedelta
+import pytz
+
+def get_stream_events():
+    events = []
+    doc = BeautifulSoup(requests.get('http://www.taketv.net/streams').text)    
+    entries = []    
+    for h2 in doc.find_all('h2'):
+        who = h2.getText().encode('utf-8')        
+        date = h2.findPrevious('span', {'class':'datum'}).getText()
+        time = h2.findPrevious('span', {'class':'time'}).getText()
+        when_dt = datetime.strptime('{0} {1}'.format(date,time), '%d.%m %H:%Mh')
+        when_dt = when_dt.replace(year=datetime.now().year)        
+        berlin = pytz.timezone('Europe/Berlin')
+        event = {
+        'summary': 'TakeTV - {0}'.format(who),        
+        'location': 'www.twitch.tv/taketv',        
+        'created': datetime.utcnow().replace(tzinfo=pytz.utc),
+        'start' : when_dt.replace(tzinfo=berlin),
+        'end' : (when_dt+timedelta(hours=2,minutes=30)).replace(tzinfo=berlin),
+        }
+        events.append( event )
+    return events
+    
+def create_calendar(events):
+    cal = Calendar()
+    cal.add('proid', 'TakeTV Streaming Dates')
+    cal.add('version', '2.0')
+    
+    for e in events:
+        event = Event()
+        event.add('summary', e['summary'])        
+        event.add('dtstart',e['start'])
+        event.add('dtend',e['end'])
+        event.add('dtstamp',e['created'])
+        event.add('location', e['location'])
+        event['uid'] = "{0}#{1}".format(e['start'],e['summary'])
+        cal.add_component(event)
+    return cal
+
+def write_to_file(path, events):
+    with open(path, 'wb') as f:
+        f.write(ical.to_ical())
+        
+if __name__ == "__main__":
+    events = get_stream_events()
+    ical = create_calendar(events)
+    write_to_file('taketv.ics', events)
